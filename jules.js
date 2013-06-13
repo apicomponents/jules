d0cf14d14ecfae93e201414be9c33a66059d1005
Ben Atkin <ben@benatkin.com>
2013-06-13 16:40:09 -0600

    build package from jules.js

--- jules.js
+++ jules.js
@@ -11,5 +11,38 @@ module.exports = {
         this.arguments.file = argv[1];
       }
     }
+  },
+  "test": {
+    "run": function() {
+    }
+  },
+  "run": function() {
+    console.log('hi!');
+  },
+  "package": {
+    "data": {
+      "name": "jules",
+      "version": "0.0.0",
+      "description": "json manipulation tool",
+      "main": "jules.js",
+      "scripts": {
+        "test": "node test"
+      },
+      "repository": {
+        "type": "git",
+        "url": "git@github.com:benatkin/jules.git"
+      },
+      "keywords": [
+        "json",
+        "manipulation",
+        "editor"
+      ],
+      "author": "Ben Atkin",
+      "license": "MIT"
+    },
+    "build": function() {
+      var fs = require('fs');
+      fs.writeFile('./package.json', JSON.stringify(this.data, null, 2) + "\n", 'utf8');
+    }
   }
 };
