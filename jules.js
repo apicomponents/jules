99342c5463f3f414e2edafd99ec5f22abd2b69ae
Ben Atkin <ben@benatkin.com>
2013-06-13 17:16:02 -0600

    put targets into separate group from the core

--- jules.js
+++ jules.js
@@ -1,48 +1,39 @@
 module.exports = {
-  "command": {
-    "argv": [],
-    "arguments": {
-      "command": "help",
-      "file": null
-    },
-    "run": function() {
-      this.argv = process.argv;
-      if (this.argv.length >= 2) {
-        this.arguments.file = argv[1];
+  "targets": {
+    "main": {
+      "build": function() {
       }
-    }
-  },
-  "test": {
-    "run": function() {
-    }
-  },
-  "run": function() {
-    console.log('hi!');
-  },
-  "package": {
-    "data": {
-      "name": "jules",
-      "version": "0.0.0",
-      "description": "json manipulation tool",
-      "main": "jules.js",
-      "scripts": {
-        "test": "node test"
-      },
-      "repository": {
-        "type": "git",
-        "url": "git@github.com:benatkin/jules.git"
-      },
-      "keywords": [
-        "json",
-        "manipulation",
-        "editor"
-      ],
-      "author": "Ben Atkin",
-      "license": "MIT"
     },
-    "build": function() {
-      var fs = require('fs');
-      fs.writeFile('./package.json', JSON.stringify(this.data, null, 2) + "\n", 'utf8');
+    "test": {
+      "run": function() {
+      }
+    },
+    "pkg": {
+      "data": {
+        "name": "jules",
+        "version": "0.0.0",
+        "description": "json manipulation tool",
+        "main": "jules.js",
+        "scripts": {
+          "pkg": "node -e \"require('./jules').targets.pkg.build()\"",
+          "test": "node -e \"require('./jules').targets.test.run()\""
+        },
+        "repository": {
+          "type": "git",
+          "url": "git@github.com:benatkin/jules.git"
+        },
+        "keywords": [
+          "json",
+          "manipulation",
+          "editor"
+        ],
+        "author": "Ben Atkin",
+        "license": "MIT"
+      },
+      "build": function() {
+        var fs = require('fs');
+        fs.writeFile('./package.json', JSON.stringify(this.data, null, 2) + "\n", 'utf8');
+      }
     }
   }
 };
