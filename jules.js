900b3692ed4422949286e9e2f63c7b1314df2aee
Ben Atkin <ben@benatkin.com>
2013-06-17 04:22:30 -0600

    generate bin script

--- jules.js
+++ jules.js
@@ -1,5 +1,21 @@
 module.exports = {
+  "build": function() {
+    this.targets.package.build();
+    this.targets.bin.build();
+  },
+  "run": function() {
+    console.log('Hello, world.');
+  },
   "targets": {
+    "bin": {
+      "src": [
+        "#!/usr/bin/env node",
+        "require('./jules').run();"
+      ],
+      "build": function() {
+        require('fs').writeFile('./bin.js', this.src.join("\n") + "\n", 'utf8');
+      }
+    },
     "main": {
       "build": function() {
       }
@@ -8,14 +24,16 @@ module.exports = {
       "run": function() {
       }
     },
-    "pkg": {
+    "package": {
       "data": {
         "name": "jules",
         "version": "0.0.0",
         "description": "json manipulation tool",
         "main": "jules.js",
+        "bin": "bin.js",
+        "dependencies": {},
         "scripts": {
-          "pkg": "node -e \"require('./jules').targets.pkg.build()\"",
+          "build": "node -e \"require('./jules').build()\"",
           "test": "node -e \"require('./jules').targets.test.run()\""
         },
         "repository": {
@@ -31,8 +49,7 @@ module.exports = {
         "license": "MIT"
       },
       "build": function() {
-        var fs = require('fs');
-        fs.writeFile('./package.json', JSON.stringify(this.data, null, 2) + "\n", 'utf8');
+        require('fs').writeFile('./package.json', JSON.stringify(this.data, null, 2) + "\n", 'utf8');
       }
     }
   }
