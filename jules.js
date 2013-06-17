a31145032ed8427e1e12889096274f539faaf4cb
Ben Atkin <ben@benatkin.com>
2013-06-17 06:47:21 -0600

    implement addressing by path

--- jules.js
+++ jules.js
@@ -3,40 +3,73 @@ module.exports = {
     this.targets.package.build();
     this.targets.bin.build();
   },
+  "usage": [
+    'Usage: jules file.json /path command [args...]',
+    '       jules file.json /path    (shows the data at path)',
+    '       jules file.json          (shows the data at the root)',
+    '',
+    'The path is a json pointer. A forward slash by itself points to the root.',
+    '',
+    'To view a list of commands, run `jules help`.',
+    'To view help about a specific command, run `jules help <command>`.'
+  ],
   "run": function() {
     var args = process.argv.slice(2);
 
     if (args.length < 1) {
-      console.log('Usage: jules file [command] [path] [args...]');
+      console.log(this.usage.join("\n"));
       process.exit();
     }
     var file = args[0];
 
-    var command_name;
+    var path;
     if (args.length < 2) {
-      command_name = 'get';
+      path = '/';
     } else {
-      command_name = args[1];
+      path = args[1];
+    }
+
+    var command_name;
+    if (args.length < 3) {
+      command_name = 'show';
+    } else {
+      command_name = args[2];
     }
 
     var command = this.commands[command_name];
     if (typeof command === 'object' && command !== null) {
       command.file = file;
-      var before = command.before;
-      if (typeof before == "string") {
-        before = this.helpers[before];
+      command.path = path;
+
+      var tasks = [];
+      var i;
+      if (Array.isArray(command.before)) {
+        for (i=0; i < command.before.length; i++) {
+          tasks.push(command.before[i]);
+        }
+      } else if (typeof command.before == 'string' || typeof command.before == 'function') {
+        tasks.push(command.before);
       }
-      if (before) {
-        if (before.length == 1) {
-          before.call(command, runCommand);
-        } else {
-          before.call(command);
-          runCommand();
+      tasks.push(command.run);
+
+      for (i=0; i < tasks.length; i++) {
+        if (typeof tasks[i] == "string") {
+          tasks[i] = this.helpers[tasks[i]];
         }
       }
 
-      function runCommand() {
-        command.run();
+      nextTask();
+
+      function nextTask() {
+        if (tasks.length == 0) return;
+
+        var task = tasks.shift();
+        if (task.length == 1) {
+          task.call(command, nextTask);
+        } else {
+          task.call(command);
+          nextTask();
+        }
       }
     } else {
       console.error('The command ' + JSON.stringify(command_name) + ' is not supported.');
@@ -51,14 +84,63 @@ module.exports = {
         that.data = JSON.parse(data);
         done();
       });
+    },
+    "parsePath": function() {
+      var path = this.path[0] == '/' ? this.path.substring(1) : this.path;
+      this.pathArray = path.length > 0 ? path.split("/") : [];
+      for (var i=0; i < this.pathArray.length; i++) {
+        this.pathArray[i] = decodeURIComponent(this.pathArray[i]);
+      }
+    },
+    "getPath": function() {
+      var node = this.data;
+      for (var i=0; i < this.pathArray.length; i++) {
+        if (node.hasOwnProperty(this.pathArray[i])) {
+          node = node[this.pathArray[i]];
+        } else {
+          return;
+        }
+      }
+      this.pathData = node;
     }
   },
   "commands": {
-    "get": {
-      "usage": "jules file get [path]",
-      "before": "readFile",
+    "show": {
+      "before": ["readFile", "parsePath", "getPath"],
       "run": function() {
-        console.log(JSON.stringify(this.data, null, 2));
+        if (typeof this.pathData !== 'undefined') {
+          console.log(JSON.stringify(this.pathData, null, 2));
+        }
+      }
+    },
+    "get": {
+      "before": ["readFile", "parsePath", "getPath"],
+      "run": function() {
+        if (typeof this.pathData !== 'undefined') {
+          console.log(JSON.stringify(this.pathData, null, 2));
+        }
+      }
+    },
+    "keys": {
+      "before": ["readFile", "parsePath", "getPath"],
+      "run": function() {
+        if (typeof this.pathData == 'object' && this.pathData !== null) {
+          var keys = Object.keys(this.pathData);
+          for (i=0; i < keys.length; i++) {
+            console.log(keys[i]);
+          }
+        }
+      }
+    },
+    "jkeys": {
+      "before": ["readFile", "parsePath", "getPath"],
+      "run": function() {
+        if (typeof this.pathData == 'object' && this.pathData !== null) {
+          var keys = Object.keys(this.pathData);
+          for (i=0; i < keys.length; i++) {
+            console.log(JSON.stringify(keys[i]));
+          }
+        }
       }
     }
   },
